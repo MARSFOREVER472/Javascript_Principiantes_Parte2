a06c4ecb85439c20f84c79e105385fe280411ee0
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-11-07 13:57:08 -0300

    Ejercicio 1 Terminado!!!!!!

---
+++ ArchivosEjecutables/exercises.js
@@ -0,0 +1,29 @@
+/*
+     JavaScript Practice Exercises For Beginners!
+     ============================================
+
+     Beginner Exercises: Part 2
+     --------------------------
+*/
+
+/* 
+   Exercise 1:
+   -----------
+
+   Write a JavaScript program to create a new string from a
+   given string taking the first 3 characters and the last 3
+   characters of a string and adding them together.
+
+   P.D. : The string length must be 3 or more, if not, the original
+   string is returned.
+
+*/
+
+const makeNewString = (str) =>
+    str.length < 3 ? str: str.slice(0, 3) + str.slice(-3);
+
+console.log(makeNewString('abc'));
+console.log(makeNewString('abcdef'));
+console.log(makeNewString('abc123abc123'));
+
+console.log(makeNewString('ab'));
